e6f97bf066375bafeed5cc5a4afcd5e694abb0dd
weiweixuan <1505497906@qq.com>
2019-10-30 16:35:56 +0800

    优化首页

---
+++ weiwei/src/store/state/home.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'roduers前端进阶',
+  subTitle: '高级前端进阶之路',
+  btn: 'Get Started',
+  showList: [
+    {
+      title: '进阶·博文',
+      content:
+        '每周重点攻克一个前端知识重难点，我们一起走进高级前端的世界，在进阶之路上，共勉！'
+    },
+    {
+      title: '每日·壹题',
+      content:
+        '工作日每天搞定一道大厂前端面试题，一年后再回头，会感谢曾经努力的自己！'
+    },
+    {
+      title: '灵感·记录',
+      content: '记录自己有灵感的问题，与大家一起探讨更优秀的写法！'
+    }
+  ],
+  copyright: 'MIT Licensed | Copyright © 2019-present Rodgers'
+}
